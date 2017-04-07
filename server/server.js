b16cf85797feb0487519aff73a26911698258614
Vlad M <vladimir.martynenko.work@gmail.com>
2017-04-07 19:15:53 +0300

    - added dummy graphql server

---
+++ server/server.js
@@ -0,0 +1,5 @@
+import SocketServer from './socket-server.js';
+import GraphQLServer from './graphql-server.js';
+
+GraphQLServer();
+new SocketServer();
--- server/socket-server.js
+++ server/socket-server.js
@@ -1,36 +1,46 @@
 import * as messageActionTypes from '../src/message/actionTypes';
 
-const WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({port: 8090});
+export default class SocketServer {
+    constructor() {
+        const WebSocketServer = require('ws').Server,
+            wss = new WebSocketServer({port: 8090});
 
-wss.on('connection', (socket) => {
-    socket.on('message', (e) => {
-        const action = JSON.parse(e);
-        handleAction(socket, action);
-    });
-});
-
-function handleAction(socket, action) {
-    let reply = {};
-    switch (action.type) {
-        case messageActionTypes.LOAD:
-            reply.type = messageActionTypes.LOADED;
-            reply.messages = [
-                {
-                    data: 'test 1'
-                }, {
-                    data: 'test 2'
-                }, {
-                    data: 'test 3'
-                }, {
-                    data: 'test 4'
-                }, {
-                    data: 'test 5'
-                }, {
-                    data: 'test 6'
-                }
-            ];
-            socket.send(JSON.stringify(reply));
-            break;
+        wss.on('connection', (socket) => {
+            new SocketClient(socket);
+        });
     }
 }
+
+class SocketClient {
+    constructor(socket) {
+        socket.on('message', (e) => {
+            const action = JSON.parse(e);
+            this.handleAction(socket, action);
+        });
+    }
+
+    handleAction(socket, action) {
+        let reply = {};
+        switch (action.type) {
+            case messageActionTypes.LOAD:
+                reply.type = messageActionTypes.LOADED;
+                reply.messages = [
+                    {
+                        data: 'test 1'
+                    }, {
+                        data: 'test 2'
+                    }, {
+                        data: 'test 3'
+                    }, {
+                        data: 'test 4'
+                    }, {
+                        data: 'test 5'
+                    }, {
+                        data: 'test 6'
+                    }
+                ];
+                socket.send(JSON.stringify(reply));
+                break;
+        }
+    }
+};
